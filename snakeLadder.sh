9d723e14fabf56b3abadcbe263d8ef5534b45850
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-30 14:52:43 +0530

    ADDED: modified to play with 2 players

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 #constants
-declare -a positionOfFirst
 declare isSnake=1
 declare isLadder=2
-declare limit=100 #change this to check with smaller value
+declare limit=20 #change this to check with smaller value
 #variables
 declare -a isWon
+declare -a positionOfFirst
 declare diceOfFirst=0
+declare -a positionOfSecond
+declare diceOfSecond=0
 
 #functions
 function rollDice(){
@@ -22,6 +24,8 @@ function checkIfWon() {
 	if [ $winner -eq 1 ]
 	then
 		position=${positionOfFirst[diceOfFirst]}
+	else
+		position=${positionOfSecond[diceOfSecond]}
 	fi
 	if [ $position -ge $limit ]
 	then
@@ -37,30 +41,58 @@ function play() {
 	then
 		currentPosition=${positionOfFirst[diceOfFirst]}
 		((diceOfFirst++))
+	else
+		currentPosition=${positionOfSecond[diceOfSecond]}
+		((diceOfSecond++))
 	fi
 	option=$( getOption )
 	case $option in
 		$isSnake)
 			newPosition=$(($currentPosition-$dice))
-			if [ $newPosition -ge 0 ]
+			if [ $currentPlayer -eq 1 ]
 			then
-				positionOfFirst[diceOfFirst]=$newPosition
+				if [ $newPosition -ge 0 ]
+				then
+					positionOfFirst[diceOfFirst]=$newPosition
+				else
+					positionOfFirst[diceOfFirst]=0
+				fi
 			else
-				positionOfFirst[diceOfFirst]=0
+				if [ $newPosition -ge 0 ]
+				then
+					positionOfSecond[diceOfSecond]=$newPosition
+				else
+					positionOfSecond[diceOfSecond]=0
+				fi
 			fi
 			;;
 		$isLadder)
 			newPosition=$(($currentPosition+$dice))
-			if [ $newPosition -le $limit ]
+			if [ $currentPlayer -eq 1 ]
 			then
-				positionOfFirst[diceOfFirst]=$newPosition
+				if [ $newPosition -le $limit ]
+				then
+					positionOfFirst[diceOfFirst]=$newPosition
+				else
+					positionOfFirst[diceOfFirst]=$currentPosition
+				fi
 			else
-				positionOfFirst[diceOfFirst]=$currentPosition
+				if [ $newPosition -le $limit ]
+				then
+					positionOfSecond[diceOfSecond]=$newPosition
+				else
+					positionOfSecond[diceOfSecond]=$currentPosition
+				fi
 			fi
 			;;
 		*)
 			newPosition=$currentPosition
-			positionOfFirst[diceOfFirst]=$newPosition
+			if [ $currentPlayer -eq 1 ]
+			then	
+				positionOfFirst[diceOfFirst]=$newPosition
+			else	
+				positionOfSecond[diceOfSecond]=$newPosition
+			fi
 			;;
 	esac
 	checkIfWon $currentPlayer
@@ -73,9 +105,11 @@ function startGame() {
 		if [ $i -eq 1 ]
 		then
 			positionOfFirst[0]=0
+		else
+			positionOfSecond[0]=0
 		fi
 	done
-	while [ ${isWon[1]} -eq 0 ]
+	while [[ ${isWon[1]} -eq 0 && ${isWon[2]} -eq 0 ]]
 	do
 		for ((j=1;j<=$numberOfPlayers;j++))
 		do
@@ -87,10 +121,15 @@ function startGame() {
 	do
 		echo turn $l position ${positionOfFirst[l]}
 	done
+	echo player 2 rolled dice $diceOfFirst times
+	for ((k=0;k<=diceOfSecond;k++))
+	do
+		echo turn $k position ${positionOfSecond[k]}
+	done
 }
 
 echo "Welcome to Snake and Ladder"
 #Echo "Number of players playing"
 #read players
-players=1
+players=2
 startGame $players
