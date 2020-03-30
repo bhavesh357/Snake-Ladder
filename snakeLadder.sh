49a0a571e979ffe9c92968d46641013d644670b7
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-30 12:38:00 +0530

    ADDED: startGame function

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,1 +1,22 @@
+#!/bin bash -x
+#constants
+declare -a positionOfFirst
+#variables
+declare -a isWon
+declare diceOfFirst=0
+function startGame() {
+	for ((i=1;i<=$1;i++))
+	do
+		isWon[i]=0
+		if [ $i -eq 1 ]
+		then
+			positionOfFirst[0]=0
+		fi
+	done
+}
+
 echo "Welcome to Snake and Ladder"
+#Echo "Number of players playing"
+#read players
+players=1
+startGame $players
